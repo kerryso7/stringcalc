3cae9c66687be22560f89965c47c7cf7138598d5
kerryso7 <30492416+kerryso7@users.noreply.github.com>
2018-08-02 18:03:12 +0800

    Updated new version

--- src/main/java/ks/util/matcher/MultipleStringMatcher.java
+++ src/main/java/ks/util/matcher/MultipleStringMatcher.java
@@ -4,15 +4,9 @@ import org.apache.commons.text.matcher.StringMatcher;
 import org.apache.commons.text.matcher.StringMatcherFactory;
 
 public class MultipleStringMatcher implements StringMatcher {
-    /** The string to match, as a character array. */
-    private StringMatcher[] strMatcher;
 
-    /**
-     * Constructor that creates a matcher from a String.
-     *
-     * @param str
-     *            the string to match, must not be null
-     */
+	private StringMatcher[] strMatcher;
+
     public MultipleStringMatcher(final String[] str) {
     	strMatcher = new StringMatcher [str.length];
     	for (int i=0; i < str.length; i++) {
